2f079a3ba6b61862f1df5888c7abaad94e14b7c6
AbdulloSodikov <sodikov.ak@gmail.com>
2021-11-16 17:06:54 +0500

    Ok

--- app/src/main/java/com/androiddevs/mvvmnewsapp/fragments/SaveNewsFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.androiddevs.mvvmnewsapp.fragments
-
-import androidx.fragment.app.Fragment
-import com.androiddevs.mvvmnewsapp.R
-
-class SaveNewsFragment : Fragment (R.layout.fragment_saved_news) {
-}
